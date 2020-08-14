3f19bf79b413d6493a7108afc4937e0801f3ebf0
Renzo Lavado Rivas <renzolavado@gmail.com>
2020-08-14 01:17:12 -0500

    fix: Detail class. * Removing "@NotNull", "@NotBlank", "@NoArgsConstructor" and "@Builder".

--- src/main/java/com/renzo/employee/business/model/api/domain/Detail.java
+++ src/main/java/com/renzo/employee/business/model/api/domain/Detail.java
@@ -1,12 +1,7 @@
 package com.renzo.employee.business.model.api.domain;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -17,16 +12,12 @@ import lombok.ToString;
  */
 
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @Getter
 @Setter
 @ToString
 public class Detail {
 
-  @NotBlank
   private String cargo;
 
-  @NotNull
   private Double salario;
 }
