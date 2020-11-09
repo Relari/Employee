2ae77909476e1d87672925a45a89708875bbae78
Renzo Lavado Rivas <renzolavado@gmail.com>
2020-11-08 19:39:38 -0500

    refactor: The EmployeeConstant class is relocated.

---
+++ src/main/java/com/renzo/employee/business/model/api/request/EmployeeConstant.java
@@ -0,0 +1,10 @@
+package com.renzo.employee.business.model.api.request;
+
+class EmployeeConstant {
+
+  private EmployeeConstant() {}
+
+  public static final String ONLY_LETTERS = "[a-zA-Z]*";
+  public static final String REGEXP_SEX = "M|F";
+
+}
--- src/main/java/com/renzo/employee/business/model/api/request/constant/EmployeeConstant.java
+++
@@ -1,9 +0,0 @@
-package com.renzo.employee.business.model.api.request.constant;
-
-public class EmployeeConstant {
-
-  public EmployeeConstant() {}
-
-  public static final String onlyLettersWithSpace = "[a-z A-Z]*";
-  public static final String regexpSex = "M|F";
-}
